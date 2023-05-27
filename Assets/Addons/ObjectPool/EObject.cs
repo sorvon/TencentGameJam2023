4fe830fb7e52b72aff4d003d05a013be6cd39673
Inoriant <424504326@qq.com>
2023-05-27 10:19:35 +0800

    加入Label属性，导入Cinemachine

--- Assets/Addons/ObjectPool/EObject.cs
+++ Assets/Addons/ObjectPool/EObject.cs
@@ -2,6 +2,26 @@ namespace Services
 {
     public enum EObject
     {
-        Cloud
+        /// <summary>
+        /// 云朵
+        /// </summary>
+        Cloud,
+        /// <summary>
+        /// 火种
+        /// </summary>
+        Kindling,
+        /// <summary>
+        /// 风筝
+        /// </summary>
+        Kite,
+        /// <summary>
+        /// 羽毛
+        /// </summary>
+        Feather,
+        /// <summary>
+        /// 星星
+        /// </summary>
+        Star
+        
     }
 }
--- Assets/Scripts/Environment/EnvGenerator.cs
+++ Assets/Scripts/Environment/EnvGenerator.cs
@@ -7,8 +7,31 @@ using UnityEngine;
 public class EnvGenerator : Service
 {
     [Other] private ObjectManager objectManager;
+    private Dictionary<EObject, bool> collectionEnables;
+
+    protected override void Start()
+    {
+        base.Start();
+        Init();
+        objectManager = ServiceLocator.Get<ObjectManager>();
+    }
+
     public void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            objectManager.Activate(EObject.Cloud, new Vector3(0, 0, 0));
+        }
+    }
+
+    private void Init()
+    {
+        collectionEnables = new Dictionary<EObject, bool>()
+        {
+            { EObject.Kite, true },
+            { EObject.Feather, false },
+            { EObject.Kindling, false },
+            { EObject.Star, false }
+        };
     }
 }
