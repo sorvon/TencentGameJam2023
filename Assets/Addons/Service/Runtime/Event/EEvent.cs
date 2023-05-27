615e1258effd122090be20d3742bcf2fe6967a00
Inoriant <424504326@qq.com>
2023-05-27 09:04:02 +0800

    导入单例轮子

---
+++ Assets/Addons/Service/Runtime/Event/EEvent.cs
@@ -0,0 +1,15 @@
+namespace Services
+{
+    public enum EEvent
+    {
+        /// <summary>
+        /// 加载场景前，参数：即将加载的场景号
+        /// </summary>
+        BeforeLoadScene,
+        /// <summary>
+        /// 加载场景后（至少一帧以后），参数：刚加载好的场景号
+        /// </summary>
+        AfterLoadScene,
+  
+    }
+}
