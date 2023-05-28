2d6f3788380e8dd37f8045076039373cc325bc2e
Inoriant <424504326@qq.com>
2023-05-28 13:48:05 +0800

    背景更改

---
+++ Assets/LevelUpTitle.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Services;
+using UnityEngine;
+
+public class LevelUpTitle : Service
+{
+    
+    public void OnLevelUp()
+    {
+        
+    }
+}
