07e34e15d16351fe75342ba4db09760e4403e827
sorvon <sorvon@qq.com>
2023-05-29 03:44:47 +0800

    apply player

---
+++ Assets/Scripts/PlayerManager/HandbookManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Services;
+using UnityEngine;
+using TMPro;
+using Spine.Unity;
+
+public class HandbookManager : MonoBehaviour 
+{
+    [SerializeField] Sprite[] handbookSprites;
+    
+    LevelManager levelManager;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Start()
+    {
+        levelManager = Services.ServiceLocator.Get<LevelManager>();
+    }
+
+    public void SetHandbookSprite(int index)
+    {
+        if (index < handbookSprites.Length)
+        {
+            spriteRenderer.sprite = handbookSprites[index];
+        }
+        else
+        {
+            Debug.LogError($"SetHandbookSprite index:{index} is out of handbookSprites Length");
+        }
+        
+    }
+}
